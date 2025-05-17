99b3c6b225c8da97093b23a24e889f12e71a1b03
Tampinha04 <hiagocanalrcc@gmail.com>
2025-05-17 16:11:46 -0300

    Add files via upload

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
+  // Ao clicar no botão de hambúrguer
   $("#mobile_btn").on("click", function () {
-    $("#mobile_menu").toggleClass("active");
+    // Alterna a classe 'ativo' para mostrar/ocultar o menu lateral
+    $("#mobile_menu").toggleClass("ativo");
+
+    // Alterna a classe 'fa-x' no ícone para mudar de hambúrguer para "X"
     $("#mobile_btn").find("i").toggleClass("fa-x");
   });
 
+  // Lógica para ativar o item de navegação com base no scroll
   const sections = $("section");
-
   $(window).on("scroll", function () {
     const scrollPosition = $(window).scrollTop() + 110;
 
-    $("section").each(function () {
+    sections.each(function () {
       const sectionTop = $(this).offset().top;
       const sectionHeight = $(this).outerHeight();
       const sectionId = $(this).attr("id");
@@ -19,10 +23,27 @@ $(document).ready(function () {
         scrollPosition < sectionTop + sectionHeight
       ) {
         $(".nav-item").removeClass("active");
-
         $(`.nav-item a[href="#${sectionId}"]`).parent().addClass("active");
       }
     });
+  });
+
+  // Adiciona rolagem suave e fecha o menu ao clicar nas opções do menu
+  $("#mobile_nav_list a").on("click", function (e) {
+    e.preventDefault(); // Impede o comportamento padrão de navegação
+
+    var target = $(this).attr("href"); // Obtém o href da âncora
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top - 80, // Ajuste a posição da rolagem
+      },
+      500, // Duração da animação
+      function () {
+        // Fecha o menu após a rolagem
+        $("#mobile_menu").removeClass("ativo");
+        $("#mobile_btn").find("i").removeClass("fa-x");
+      }
+    );
   });
 
   // ScrollReveal
